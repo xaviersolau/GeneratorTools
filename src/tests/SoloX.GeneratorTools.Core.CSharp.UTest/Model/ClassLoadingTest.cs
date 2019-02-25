acdd43461bd1e974a635d94706ac37f29c00e52b
Xavier Solau <xaviersolau@yahoo.fr>
2019-02-25 08:58:49 +0100

    Add class loading test case.

--- src/tests/SoloX.GeneratorTools.Core.CSharp.UTest/Model/ClassLoadingTest.cs
+++ src/tests/SoloX.GeneratorTools.Core.CSharp.UTest/Model/ClassLoadingTest.cs
@@ -13,6 +13,7 @@ using Moq;
 using SoloX.GeneratorTools.Core.CSharp.Model;
 using SoloX.GeneratorTools.Core.CSharp.Model.Impl;
 using SoloX.GeneratorTools.Core.CSharp.Model.Resolver;
+using SoloX.GeneratorTools.Core.CSharp.Model.Use;
 using SoloX.GeneratorTools.Core.CSharp.Model.Use.Impl;
 using SoloX.GeneratorTools.Core.CSharp.Workspace.Impl;
 using Xunit;
@@ -22,9 +23,12 @@ namespace SoloX.GeneratorTools.Core.CSharp.UTest.Model
     public class ClassLoadingTest
     {
         [Theory]
-        [InlineData("./Resources/Model/SimpleClass.cs", null, null)]
-        [InlineData("./Resources/Model/SimpleClassWithBase.cs", null, "SimpleClass")]
-        [InlineData("./Resources/Model/GenericClass.cs", "T", null)]
+        [InlineData("./Resources/Model/Basic/SimpleClass.cs", null, null)]
+        [InlineData("./Resources/Model/Basic/SimpleClassWithBase.cs", null, "SimpleClass")]
+        [InlineData("./Resources/Model/Basic/SimpleClassWithGenericBase.cs", null, "GenericClass")]
+        [InlineData("./Resources/Model/Basic/GenericClass.cs", "T", null)]
+        [InlineData("./Resources/Model/Basic/GenericClassWithBase.cs", "T", "SimpleClass")]
+        [InlineData("./Resources/Model/Basic/GenericClassWithGenericBase.cs", "T", "GenericClass")]
         public void LoadCSharpClassTest(string file, string typeParameterName, string baseClassName)
         {
             var csFile = new CSharpFile(file);
@@ -59,6 +63,38 @@ namespace SoloX.GeneratorTools.Core.CSharp.UTest.Model
                 var baseClass = Assert.Single(classDecl.Extends);
                 Assert.Equal(baseClassName, baseClass.Declaration.Name);
             }
+        }
+
+        [Theory]
+        [InlineData("./Resources/Model/Basic/GenericClassWithBase.cs", "./Resources/Model/Basic/SimpleClass.cs")]
+        [InlineData("./Resources/Model/Basic/GenericClassWithGenericBase.cs", "./Resources/Model/Basic/GenericClass.cs")]
+        public void LoadExtendsTest(string file, string baseFile)
+        {
+            var csBaseFile = new CSharpFile(baseFile);
+            csBaseFile.Load();
+            var baseDeclarationSingle = Assert.Single(csBaseFile.Declarations);
+            var baseDeclaration = Assert.IsType<ClassDeclaration>(baseDeclarationSingle);
+
+            var csFile = new CSharpFile(file);
+            csFile.Load();
+
+            var declaration = Assert.Single(csFile.Declarations);
+
+            var declarationResolverMock = new Mock<IDeclarationResolver>();
+            declarationResolverMock.Setup(dr => dr.Resolve(baseDeclaration.Name, declaration)).Returns(baseDeclaration);
+            declarationResolverMock.Setup(dr => dr.Resolve(baseDeclaration.Name, It.IsAny<IReadOnlyList<IDeclarationUse>>(), declaration)).Returns(baseDeclaration);
+
+            var classDecl = Assert.IsType<ClassDeclaration>(declaration);
+
+            classDecl.Load(declarationResolverMock.Object);
+
+            Assert.NotNull(classDecl.GenericParameters);
+            Assert.NotNull(classDecl.Extends);
+
+            var extendsSingle = Assert.Single(classDecl.Extends);
+
+            var genDeclUse = Assert.IsType<GenericDeclarationUse>(extendsSingle);
+            Assert.Same(baseDeclaration, genDeclUse.Declaration);
         }
     }
 }
