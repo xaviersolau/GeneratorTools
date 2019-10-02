bc479fe09135b800cbd45d90bbaebcceacceeb33
Xavier Solau <xaviersolau@yahoo.fr>
2019-10-02 19:59:43 +0200

    Add a generic declaration resolver test.

--- src/tests/SoloX.GeneratorTools.Core.CSharp.UTest/Model/Resolver/DeclarationResolverTest.cs
+++ src/tests/SoloX.GeneratorTools.Core.CSharp.UTest/Model/Resolver/DeclarationResolverTest.cs
@@ -13,6 +13,7 @@ using Moq;
 using SoloX.GeneratorTools.Core.CSharp.Model;
 using SoloX.GeneratorTools.Core.CSharp.Model.Impl;
 using SoloX.GeneratorTools.Core.CSharp.Model.Resolver.Impl;
+using SoloX.GeneratorTools.Core.CSharp.Model.Use;
 using SoloX.GeneratorTools.Core.CSharp.UTest.Utils;
 using SoloX.GeneratorTools.Core.CSharp.Workspace;
 using Xunit;
@@ -66,6 +67,61 @@ namespace SoloX.GeneratorTools.Core.CSharp.UTest.Model.Resolver
             else
             {
                 Assert.Null(resolvedDecl);
+            }
+        }
+
+        [Theory]
+        [InlineData("ctxNameSpace", "name", "paramName", "ctxNameSpace", null, "name", true)]
+        [InlineData("ctxNameSpace", "name", "paramName", "ctxNameSpace", null, "ctxNameSpace.name", true)]
+        [InlineData("a.b.c", "name", "paramName", "ctxNameSpace", "a.b.c", "name", true)]
+        [InlineData("a.b.c", "name", "paramName", "ctxNameSpace", "a.b", "c.name", true)]
+        [InlineData("a.b.c", "name", "paramName", "ctxNameSpace", null, "a.b.c.name", true)]
+        [InlineData("a.b.c", "name", "paramName", "ctxNameSpace", null, "name", false)]
+        [InlineData("a.b.c", "name", "paramName", "ctxNameSpace", "a.b.c", "unknown", false)]
+        [InlineData("ctxNameSpace", "name", "paramName", "ctxNameSpace.a", null, "name", true)]
+        public void ResolveGenericDeclarationTest(
+            string nameSpace, string name, string nameParam, string ctxNameSpace, string usingNameSpace, string nameToResolve, bool expectedMatch)
+        {
+            var genericParameter = Mock.Of<IGenericParameterDeclaration>();
+            var genericDeclaration = DeclarationHelper.SetupDeclaration<IGenericDeclaration<SyntaxNode>>(nameSpace, name, mock =>
+            {
+                mock.SetupGet(x => x.GenericParameters).Returns(new[] { genericParameter });
+            });
+
+            var contextDeclaration = DeclarationHelper.SetupDeclaration<IDeclaration<SyntaxNode>>(ctxNameSpace, "ctxName", m =>
+            {
+                var usingDirectives = string.IsNullOrEmpty(usingNameSpace) ? Array.Empty<string>() : new[] { usingNameSpace };
+                m.SetupGet(d => d.UsingDirectives).Returns(usingDirectives);
+            });
+
+            var declarationParam = DeclarationHelper.SetupDeclaration<IDeclaration<SyntaxNode>>(nameSpace, nameParam);
+            var declarationParamUse1 = DeclarationHelper.SetupDeclarationUse<IDeclarationUse<SyntaxNode>>(declarationParam);
+            var declarationParamUse2 = DeclarationHelper.SetupDeclarationUse<IDeclarationUse<SyntaxNode>>(declarationParam);
+
+            var declResolver = new DeclarationResolver(new[] { genericDeclaration }, (r, d) => { });
+
+            var resolvedGenericDecl1 = declResolver.Resolve(
+                nameToResolve,
+                new[] { declarationParamUse1 },
+                contextDeclaration);
+            var resolvedGenericDecl2 = declResolver.Resolve(
+                nameToResolve,
+                new[] { declarationParamUse1, declarationParamUse2 },
+                contextDeclaration);
+
+            var resolvedDecl = declResolver.Resolve(nameToResolve, contextDeclaration);
+
+            Assert.Null(resolvedDecl);
+            Assert.Null(resolvedGenericDecl2);
+
+            if (expectedMatch)
+            {
+                Assert.NotNull(resolvedGenericDecl1);
+                Assert.Same(genericDeclaration, resolvedGenericDecl1);
+            }
+            else
+            {
+                Assert.Null(resolvedGenericDecl1);
             }
         }
     }
