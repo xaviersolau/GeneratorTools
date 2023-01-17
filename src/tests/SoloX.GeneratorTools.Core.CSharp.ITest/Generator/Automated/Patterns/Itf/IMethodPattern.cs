f07e880f768b82ab7053314c6cb97493cfa36e02
Xavier Solau <xaviersolau@yahoo.fr>
2023-01-17 23:26:54 +0100

    Add no repeat class pattern.

---
+++ src/tests/SoloX.GeneratorTools.Core.CSharp.ITest/Generator/Automated/Patterns/Itf/IMethodPattern.cs
@@ -0,0 +1,22 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="IMethodPattern.cs" company="Xavier Solau">
+// Copyright © 2021 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using SoloX.GeneratorTools.Core.CSharp.Generator.Attributes;
+
+namespace SoloX.GeneratorTools.Core.CSharp.ITest.Generator.Automated.Patterns.Itf
+{
+
+    [Pattern(typeof(MultiSelector))]
+    public interface IMethodPattern
+    {
+        [Repeat(Pattern = nameof(ISimplePattern), Prefix = "I")]
+        ISimplePattern CreateSimplePattern(
+            [Repeat(Pattern = nameof(ISimplePattern.PatternProperty))]
+            object patternProperty);
+    }
+}
